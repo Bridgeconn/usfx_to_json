37f3d51bbd07f510fea4f47c3a665a04603b721c
Sony Zacharia <sony.z@bridgeconn.com>
2016-03-23 15:22:23 +0530

    toc values

--- convertor.js
+++ convertor.js
@@ -15,35 +15,52 @@ books_var = etree.findall('book');
 res.langCode = etree.find('languageCode').text;
 res.books = [];
 books_var.forEach(function(book, index, array) {
-    val = book.findall('p');
-    temp = {};
-    temp.name = book.attrib.id;
-    temp.chapters = [];
-    c_counter = 0;
-    var ch = {}
+  val = book.findall('p');
+  temp = {};
 
-    val.forEach(function(p, i, arr) {
-	if(p._children.length > 0) {
-	    p._children.forEach(function(v, i, varr) {
-		if(v.tag == 'v') {
-		    t = v.attrib.bcv.split('.');
-		    if(c_counter != t[1]) {
-			ch[parseInt(t[1])] = {};
-			c_counter = t[1];
-		    }
-		    ch[parseInt(t[1])][parseInt(t[2])] = v.tail.replace(/^\s+|\s+$/g, '');
-		}
-	    });
-	}
-    });
-    temp.chapters.push(ch);
-    res.books.push(temp);
+  temp.toc = [];
+  var tl = {};
+
+  temp2 = [];
+console.log(book.findall('toc'));
+    if(book._children.length>0){
+      book._children.forEach(function(toc, l, m){
+        if (toc.tag == 'toc') {
+           tl.level = toc.attrib.level;
+           tl.text = toc.text;
+          temp2.push(tl);
+          //console.log(temp2);
+        }
+      })
+      // temp.toc.push(tl);
+    };
+
+  temp.name = book.attrib.id;
+  temp.chapters = [];
+  c_counter = 0;
+  var ch = {}
+
+  val.forEach(function(p, i, arr) {
+    if(p._children.length > 0) {
+      p._children.forEach(function(v, i, varr) {
+        if(v.tag == 'v') {
+            t = v.attrib.bcv.split('.');
+            if(c_counter != t[1]) {
+              ch[parseInt(t[1])] = {};
+              c_counter = t[1];
+            }
+            ch[parseInt(t[1])][parseInt(t[2])] = v.tail.replace(/^\s+|\s+$/g, '');
+        }
+      });
+    }
+  });
+  temp.chapters.push(ch);
+  res.books.push(temp);
 });
-console.log(res);
+console.log(res.books[0].toc);
 /*
 console.log(etree.findall('./entry/TenantId').length); // 2
 console.log(etree.findtext('./entry/ServiceName')); // MaaS
 console.log(etree.findall('./entry/category')[0].get('term')); // monitoring.entity.create
 console.log(etree.findall('/category/[@term="monitoring.entity.update"]').length); // 1
 */
-// my changes
