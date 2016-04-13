199f690b4a4615e04ab0704c2a44f0bfdf4d1b40
Sony Zacharia <sony.z@bridgeconn.com>
2016-04-13 16:40:52 +0530

    converts data from url

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
 var express = require('express');
 var app = express();
 
-var convertor = require('convertor');
+var convertor = require('./convertor.js');
 
-app.get('/usfx_to_json/usx', function(req, res){
+// initialize the container for our data
+var data = "";
+
+app.get('/', function(req, res){
 
   var http = require("http");
 
   var options = {
-      host: 'www.operationagape.com' ,
-      path: '/soveetest/Bible_edited.xml'
+    host: 'www.operationagape.com' ,
+    path: '/soveetest/Bible_edited.xml'
   };
 
   http.get(options, function (http_res) {
-      // initialize the container for our data
-      var data = "";
-
-      // this event fires many times, each time collecting another piece of the response
-      http_res.on("data", function (chunk) {
-          // append this chunk to our growing `data` var
-          data += chunk;
-      });
-
-      // this event fires *one* time, after all the `data` events/chunks have been gathered
-      http_res.on("end", function () {
-          // you can use res.send instead of console.log to output via express
-          res.send(data);
-          console.log(data);
-          console.log('###########end########');
-      });
+    // this event fires many times, each time collecting another piece of the response
+    http_res.on("data", function (chunk) {
+      // append this chunk to our growing `data` var
+      data += chunk;
+    });
+    // this event fires *one* time, after all the `data` events/chunks have been gathered
+    http_res.on("end", function () {
+      convertor.processData(data)
+    });
   });
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+module.exports = app;
--- convertor.js
+++ convertor.js
@@ -8,52 +8,54 @@ var element = et.Element;
 var subElement = et.SubElement;
 
 var data, etree;
-var res = {};
-data = fs.readFileSync('./data/test.xml').toString();
-etree = et.parse(data);
-books_var = etree.findall('book');
-res.langCode = etree.find('languageCode').text;
-res.books = [];
-books_var.forEach(function(book, index, array) {
 
-  val = book.findall('p');
-  toc_val = book.findall('toc');
-  temp = {};
+exports.processData = function (data) {
+  var res = {};
+  // var data = fs.readFileSync('./data/test.xml').toString();
+  etree = et.parse(data);
+  books_var = etree.findall('book');
+  res.langCode = etree.find('languageCode').text;
+  res.books = [];
+  books_var.forEach(function(book, index, array) {
 
-  temp.toc = [];
+    val = book.findall('p');
+    toc_val = book.findall('toc');
+    temp = {};
 
-  toc_val.forEach(function(toc, i, varr) {
-  	tl = {};
-  	tl = toc.attrib;
-  	tl.text = toc.text;
-  	temp.toc.push(tl);
-    });
+    temp.toc = [];
 
-  temp.name = book.attrib.id;
-  temp.chapters = [];
-  c_counter = 0;
-  var ch = {}
-
-  val.forEach(function(p, i, arr) {
-    if(p._children.length > 0) {
-      p._children.forEach(function(v, i, varr) {
-        if(v.tag == 'v') {
-          t = v.attrib.bcv.split('.');
-          if(c_counter != t[1]) {
-            ch[parseInt(t[1])] = {};
-            c_counter = t[1];
-          }
-          ch[parseInt(t[1])][parseInt(t[2])] = v.tail.replace(/^\s+|\s+$/g, '');
-        }
+    toc_val.forEach(function(toc, i, varr) {
+      tl = {};
+      tl = toc.attrib;
+      tl.text = toc.text;
+      temp.toc.push(tl);
       });
-    }
-  });
-  temp.chapters.push(ch);
-  res.books.push(temp);
-});
-console.log(res);
 
-module.exports = convertor;
+    temp.name = book.attrib.id;
+    temp.chapters = [];
+    c_counter = 0;
+    var ch = {}
+
+    val.forEach(function(p, i, arr) {
+      if(p._children.length > 0) {
+        p._children.forEach(function(v, i, varr) {
+          if(v.tag == 'v') {
+            t = v.attrib.bcv.split('.');
+            if(c_counter != t[1]) {
+              ch[parseInt(t[1])] = {};
+              c_counter = t[1];
+            }
+            ch[parseInt(t[1])][parseInt(t[2])] = v.tail.replace(/^\s+|\s+$/g, '');
+          }
+        });
+      }
+    });
+    temp.chapters.push(ch);
+    res.books.push(temp);
+  });
+  console.log(res);
+  return res;
+}
 /*
 console.log(etree.findall('./entry/TenantId').length); // 2
 console.log(etree.findtext('./entry/ServiceName')); // MaaS
